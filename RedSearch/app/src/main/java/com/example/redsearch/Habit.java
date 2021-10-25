c47119d2397a9c65b7e77d0a0bc7e34d58b34c64
evandermeer84 <evanderm@ualberta.ca>
2021-10-25 15:33:59 -0600

    Created the habit class, started the habitlist class, I think that I have linked the firestore databse, but more investigation and testing will need to be done to be sure, I followed the online instructions from lab 5, the habits are in another acticity which will need to be linked in order to view, though it is linked in the andoird manifest file, just need to make a button on the main activity screen to link them, unless we want to start with the login, though that could simply be part of main activity I think

--- RedSearch/app/src/main/java/com/example/redsearch/Habit.java
+++ RedSearch/app/src/main/java/com/example/redsearch/Habit.java
@@ -1,5 +1,55 @@
 package com.example.redsearch;
 
+import android.graphics.Color;
+import android.os.Build;
+
+import androidx.annotation.RequiresApi;
+
+import java.util.Date;
+
 public class Habit {
+    private String title;
+    private String reason;
+    private Date startDate;
+    private Float Daysplanned;
+    private Float Dayshappened;
+    private Color color;
+
+    Habit(String title, String reason, Date date){
+        // a simple habit constructor
+        this.title = title;
+        this.reason = reason;
+        this.startDate = date;
+    }
+
+    //getters for each of the states of the object
+    String getTitle(){return this.title;}
+    String getReason(){return this.reason;}
+    Date getStartDate(){return this.startDate;}
+    Float getDaysplanned(){return this.Daysplanned;}
+    Float getDayshappened(){return  this.Dayshappened;}
+    Color getColor(){return this.color;}
+
+    //Setting the states of the object
+    public void setTitle(String Title){this.title = Title;}
+    public void setReason(String reason){this.reason = reason;}
+    public void setStartDate(Date date){this.startDate = date;}
+    public void setDaysplanned(Float daysplanned){this.Daysplanned = daysplanned;}
+    public void setDayshappened(Float dayshappened){this.Dayshappened = dayshappened;}
+    @RequiresApi(api = Build.VERSION_CODES.O) // Made me put this in to do the colour part
+    public void setColor(){
+        Float percent = this.Dayshappened/this.Daysplanned;
+        Float green = percent * 255; // This finds the value of the green part of the colour
+        Float red = 255 - green; // This finds the value of the blue part of the colour
+        Float blue = Float.valueOf(0);
+        this.color = Color.valueOf(red,green,blue);
+    }
+
+    //Resetting the details of the habit
+    public void setHabit(String title, String reason, Date date){
+        setTitle(title);
+        setReason(reason);
+        setStartDate(date);
+    }
 
 }
---
+++ RedSearch/app/src/main/java/com/example/redsearch/HabitList.java
@@ -0,0 +1,18 @@
+package com.example.redsearch;
+
+import android.content.Context;
+import android.widget.ArrayAdapter;
+
+import java.util.ArrayList;
+
+public class HabitList extends ArrayAdapter<Habit> {
+    private ArrayList<Habit> habits;
+    private Context context;
+
+    public HabitList(Context context, ArrayList<Habit> habits){
+        // This is the general constructor
+        super(context, 0, habits);
+        this.habits = habits;
+        this.context = context;
+    }
+}
