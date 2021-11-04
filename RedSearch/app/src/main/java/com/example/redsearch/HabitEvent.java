6615eae65355a72126bf18bbf4636c705a5bde22
Sam <sam.r.okeefe@gmail.com>
2021-11-04 12:36:28 -0600

    Added HabitEvent Getter

--- RedSearch/app/src/main/java/com/example/redsearch/HabitEvent.java
+++ RedSearch/app/src/main/java/com/example/redsearch/HabitEvent.java
@@ -1,10 +1,9 @@
 package com.example.redsearch;
 
-import android.graphics.Bitmap;
-
 import androidx.annotation.NonNull;
 
 import java.util.Date;
+import android.graphics.Bitmap;
 
 public class HabitEvent{
     private String comment;
@@ -60,4 +59,22 @@ public class HabitEvent{
      * @param Image {@code Bitmap} image associated with HabitEvent
      */
     private void setImage(Bitmap Image) {this.image = Image;}
+
+    /**
+     * HabitEvent date getter
+     * @return {@code Date} Date the habitEvent was created
+     */
+    Date getDate(){return this.date;}
+
+    /**
+     * HabitEvent comment getter
+     * @return {@code String} Comment associated with HabitEvent
+     */
+    String getComment(){return this.comment;}
+
+    /**
+     * HabitEvent image getter
+     * @return {@code Bitmap} Image associated with HabitEvent
+     */
+    Bitmap getImage(){return this.image;}
 }
