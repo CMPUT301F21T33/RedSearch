0b040b59f667c3e0ae8917d17eb8a0c588eb79a4
Lauren Herman <thisisjustfabulous.0@gmail.com>
2021-11-05 14:47:11 -0600

    Added view habit info

--- RedSearch/app/src/main/java/com/example/redsearch/MyHabitsActivity.java
+++ RedSearch/app/src/main/java/com/example/redsearch/MyHabitsActivity.java
@@ -6,12 +6,18 @@ import androidx.appcompat.widget.Toolbar;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import java.util.ArrayList;
 import java.util.Date;
 
 public class MyHabitsActivity extends AppCompatActivity {
+
+    public static final String TITLE = "com.example.redsearch.TITLE";
+    public static final String REASON = "com.example.redsearch.REASON";
+    public static final String DATE = "com.example.redsearch.DATE";
+    public static final String WEEKDAYS = "com.example.redsearch.WEEKDAYS";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,12 +38,32 @@ public class MyHabitsActivity extends AppCompatActivity {
         //pull data from db and put in myHabits
 
         //Habit testHab = new Habit("TestHabit","Reason", new Date(), true);
-        myHabits.add(new Habit("Drink water","Reason", new Date(), true));
-        myHabits.add(new Habit("Do laundry","Reason", new Date(), true));
+        myHabits.add(new Habit("Drink water","thirsty", new Date(), true));
+        myHabits.add(new Habit("Do laundry","dirty", new Date(), true));
 
         MyHabitList adapter = new MyHabitList(this,myHabits);
 
         list.setAdapter(adapter);
+
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String title = myHabits.get(i).getTitle();
+                String reason = myHabits.get(i).getReason();
+                String date = myHabits.get(i).getStartDate().toString();
+                //String weekdays = myHabits.get(i).get
+
+                goToViewHabit(view, title, reason, date);
+            }
+        });
+    }
+
+    public void goToViewHabit(View view, String title, String reason, String date){
+        Intent intent = new Intent(this, ViewHabitActivity.class);
+        intent.putExtra(TITLE, title);
+        intent.putExtra(REASON, reason);
+        intent.putExtra(DATE, date);
+        startActivity(intent);
     }
 
     public void goToAddHabit(View view) {
--- RedSearch/app/src/main/java/com/example/redsearch/ViewHabitActivity.java
+++ RedSearch/app/src/main/java/com/example/redsearch/ViewHabitActivity.java
@@ -2,13 +2,33 @@ package com.example.redsearch;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ViewHabitActivity extends AppCompatActivity {
+
+    TextView titleText;
+    TextView reasonText;
+    TextView dateText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_habit);
+
+
+        Intent intent = getIntent();
+        String title = intent.getStringExtra(MyHabitsActivity.TITLE); // This is the username that the user g
+        String reason = intent.getStringExtra(MyHabitsActivity.REASON);
+        String date = intent.getStringExtra(MyHabitsActivity.DATE);
+
+        titleText = findViewById(R.id.titleText);
+        reasonText = findViewById(R.id.reasonText);
+        dateText = findViewById(R.id.dateText);
+
+        titleText.setText(title);
+        reasonText.setText(reason);
+        dateText.setText(date);
     }
 }
