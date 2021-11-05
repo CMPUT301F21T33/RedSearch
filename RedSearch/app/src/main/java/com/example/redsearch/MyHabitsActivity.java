588898db8c448123d7742e08b97a9fcb64185486
Lauren Herman <thisisjustfabulous.0@gmail.com>
2021-11-05 11:05:16 -0600

    Populated My Habit with test habits (no db)

--- RedSearch/app/src/main/java/com/example/redsearch/MyHabitsActivity.java
+++ RedSearch/app/src/main/java/com/example/redsearch/MyHabitsActivity.java
@@ -6,6 +6,10 @@ import androidx.appcompat.widget.Toolbar;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Date;
 
 public class MyHabitsActivity extends AppCompatActivity {
 
@@ -17,6 +21,23 @@ public class MyHabitsActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar_my_habits);
         setSupportActionBar(toolbar);
         setTitle("My Habits");
+
+        ListView list = (ListView) findViewById(R.id.listView);
+        ArrayList<Habit> myHabits = new ArrayList<Habit>();
+
+        /*
+        db = FirebaseFirestore.getInstance();
+        final CollectionReference collectionReference = db.collection("Users");*/
+
+        //pull data from db and put in myHabits
+
+        //Habit testHab = new Habit("TestHabit","Reason", new Date(), true);
+        myHabits.add(new Habit("TestHabit","Reason", new Date(), true));
+        myHabits.add(new Habit("TestHabit2","Reason", new Date(), true));
+
+        MyHabitList adapter = new MyHabitList(this,myHabits);
+
+        list.setAdapter(adapter);
     }
 
     public void goToAddHabit(View view) {
