2a2515baf7ef535c735ad01dac606d62024530d1
srokeefe <60757194+srokeefe@users.noreply.github.com>
2021-11-29 15:41:48 -0700

    javadoc

--- RedSearch/app/src/androidTest/java/com/example/redsearch/HomeActivityTest.java
+++ RedSearch/app/src/androidTest/java/com/example/redsearch/HomeActivityTest.java
@@ -1,3 +1,32 @@
+/**
+ * CMPUT 301 FALL 2021
+ * RedSearch Project
+ * @author Sam
+ *
+ * The {@code HomeActivityTest} Class
+ *
+ * Test class for HomeActivityTest. All the UI tests are written here. Espresso test framework is
+ * used
+ *
+ * Copyright 2021
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Acknowledgements:
+ * https://developer.android.com/training/testing/espresso/intents
+ * https://www.vogella.com/tutorials/AndroidTestingEspresso/article.html#tutorial_unittestactivity
+ * https://stackoverflow.com/questions/25998659/espresso-how-can-i-check-if-an-activity-is-launched-after-performing-a-certain
+ * */
 package com.example.redsearch;
 
 import static androidx.test.espresso.Espresso.onView;
@@ -22,10 +51,6 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-/**
- * Test class for HomeActivity. All the UI tests are written here. Espresso test framework is
- used
- */
 @RunWith(AndroidJUnit4.class)
 public class HomeActivityTest {
     @Rule
