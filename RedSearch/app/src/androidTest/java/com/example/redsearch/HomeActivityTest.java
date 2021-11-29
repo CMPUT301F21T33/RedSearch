6195780a630b0448b399f9e7e0f04e724b8ce03d
srokeefe <60757194+srokeefe@users.noreply.github.com>
2021-11-29 12:15:40 -0700

    UI test

---
+++ RedSearch/app/src/androidTest/java/com/example/redsearch/HomeActivityTest.java
@@ -0,0 +1,38 @@
+package com.example.redsearch;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.intent.Intents.intended;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * Test class for HomeActivity. All the UI tests are written here. Espresso test framework is
+ used
+ */
+@RunWith(AndroidJUnit4.class)
+public class HomeActivityTest {
+    @Rule
+    public ActivityScenarioRule<MainActivity> activityRule =
+            new ActivityScenarioRule<>(MainActivity.class);
+
+    @Test
+    public void testViewAllButton(){
+
+        // Check that the activity changed to
+        intended(hasComponent(HomeActivity.class.getName()));
+    }
+
+}
